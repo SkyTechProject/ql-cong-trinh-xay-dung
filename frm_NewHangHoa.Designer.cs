577266baa3a5e3f9d14d2d6929f288a260b93088
nguyenlethanhlinh95 <33345239+nguyenlethanhlinh95@users.noreply.github.com>
2019-09-14 23:54:01 +0700

    Update code form frm_NewHangHoa

---
+++ frm_NewHangHoa.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace QL_Duan
+{
+    partial class frm_NewHangHoa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if ( disposing && (components != null) )
+            {
+                components.Dispose( );
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent( )
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_NewHangHoa));
+            this.labelControl7 = new DevExpress.XtraEditors.LabelControl();
+            this.txt_Ghichu = new DevExpress.XtraEditors.TextEdit();
+            this.btn_Exit = new DevExpress.XtraEditors.SimpleButton();
+            this.btn_Save = new DevExpress.XtraEditors.SimpleButton();
+            this.labelControl10 = new DevExpress.XtraEditors.LabelControl();
+            this.txt_DVT = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl6 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.txt_GiaNhap = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.txt_TenHH = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.txt_MaHH = new DevExpress.XtraEditors.TextEdit();
+            this.cmb_NCC = new DevExpress.XtraEditors.ComboBoxEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.txt_Ghichu.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txt_DVT.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txt_GiaNhap.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txt_TenHH.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txt_MaHH.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cmb_NCC.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelControl7
+            // 
+            this.labelControl7.Appearance.Font = new System.Drawing.Font("Tahoma", 9.5F);
+            this.labelControl7.Appearance.Options.UseFont = true;
+            this.labelControl7.Location = new System.Drawing.Point(46, 187);
+            this.labelControl7.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.labelControl7.Name = "labelControl7";
+            this.labelControl7.Size = new System.Drawing.Size(42, 16);
+            this.labelControl7.TabIndex = 147;
+            this.labelControl7.Text = "Ghi chú";
+            // 
+            // txt_Ghichu
+            // 
+            this.txt_Ghichu.Location = new System.Drawing.Point(170, 184);
+            this.txt_Ghichu.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_Ghichu.Name = "txt_Ghichu";
+            this.txt_Ghichu.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 9.5F);
+            this.txt_Ghichu.Properties.Appearance.Options.UseFont = true;
+            this.txt_Ghichu.Size = new System.Drawing.Size(459, 22);
+            this.txt_Ghichu.TabIndex = 6;
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btn_Exit.Appearance.BackColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btn_Exit.Appearance.Font = new System.Drawing.Font("Tahoma", 11F, System.Drawing.FontStyle.Bold);
+            this.btn_Exit.Appearance.ForeColor = System.Drawing.Color.White;
+            this.btn_Exit.Appearance.Options.UseBackColor = true;
+            this.btn_Exit.Appearance.Options.UseFont = true;
+            this.btn_Exit.Appearance.Options.UseForeColor = true;
+            this.btn_Exit.ImageOptions.AllowGlyphSkinning = DevExpress.Utils.DefaultBoolean.True;
+            this.btn_Exit.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btn_Exit.ImageOptions.Image")));
+            this.btn_Exit.Location = new System.Drawing.Point(401, 227);
+            this.btn_Exit.LookAndFeel.SkinMaskColor = System.Drawing.Color.Gray;
+            this.btn_Exit.LookAndFeel.SkinMaskColor2 = System.Drawing.Color.Black;
+            this.btn_Exit.LookAndFeel.SkinName = "Office 2010 Black";
+            this.btn_Exit.LookAndFeel.UseDefaultLookAndFeel = false;
+            this.btn_Exit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(110, 36);
+            this.btn_Exit.TabIndex = 9;
+            this.btn_Exit.Text = "Đóng";
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btn_Save.Appearance.BackColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btn_Save.Appearance.Font = new System.Drawing.Font("Tahoma", 11F, System.Drawing.FontStyle.Bold);
+            this.btn_Save.Appearance.ForeColor = System.Drawing.Color.White;
+            this.btn_Save.Appearance.Options.UseBackColor = true;
+            this.btn_Save.Appearance.Options.UseFont = true;
+            this.btn_Save.Appearance.Options.UseForeColor = true;
+            this.btn_Save.ImageOptions.AllowGlyphSkinning = DevExpress.Utils.DefaultBoolean.True;
+            this.btn_Save.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btn_Save.ImageOptions.Image")));
+            this.btn_Save.Location = new System.Drawing.Point(271, 227);
+            this.btn_Save.LookAndFeel.SkinMaskColor = System.Drawing.Color.RoyalBlue;
+            this.btn_Save.LookAndFeel.SkinMaskColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btn_Save.LookAndFeel.SkinName = "Office 2010 Black";
+            this.btn_Save.LookAndFeel.UseDefaultLookAndFeel = false;
+            this.btn_Save.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(110, 36);
+            this.btn_Save.TabIndex = 7;
+            this.btn_Save.Text = "Lưu";
+            // 
+            // labelControl10
+            // 
+            this.labelControl10.Appearance.Font = new System.Drawing.Font("Tahoma", 9.5F);
+            this.labelControl10.Appearance.Options.UseFont = true;
+            this.labelControl10.Location = new System.Drawing.Point(46, 127);
+            this.labelControl10.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.labelControl10.Name = "labelControl10";
+            this.labelControl10.Size = new System.Drawing.Size(61, 16);
+            this.labelControl10.TabIndex = 144;
+            this.labelControl10.Text = "Đơn vị tính";
+            // 
+            // txt_DVT
+            // 
+            this.txt_DVT.Location = new System.Drawing.Point(170, 124);
+            this.txt_DVT.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_DVT.Name = "txt_DVT";
+            this.txt_DVT.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 9.5F);
+            this.txt_DVT.Properties.Appearance.Options.UseFont = true;
+            this.txt_DVT.Size = new System.Drawing.Size(291, 22);
+            this.txt_DVT.TabIndex = 4;
+            // 
+            // labelControl6
+            // 
+            this.labelControl6.Appearance.Font = new System.Drawing.Font("Tahoma", 9.5F);
+            this.labelControl6.Appearance.Options.UseFont = true;
+            this.labelControl6.Location = new System.Drawing.Point(46, 70);
+            this.labelControl6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.labelControl6.Name = "labelControl6";
+            this.labelControl6.Size = new System.Drawing.Size(79, 16);
+            this.labelControl6.TabIndex = 143;
+            this.labelControl6.Text = "Tên hàng hóa";
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Appearance.Font = new System.Drawing.Font("Tahoma", 9.5F);
+            this.labelControl3.Appearance.Options.UseFont = true;
+            this.labelControl3.Location = new System.Drawing.Point(46, 157);
+            this.labelControl3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(50, 16);
+            this.labelControl3.TabIndex = 140;
+            this.labelControl3.Text = "Giá nhập";
+            // 
+            // txt_GiaNhap
+            // 
+            this.txt_GiaNhap.Location = new System.Drawing.Point(170, 154);
+            this.txt_GiaNhap.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_GiaNhap.Name = "txt_GiaNhap";
+            this.txt_GiaNhap.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 9.5F);
+            this.txt_GiaNhap.Properties.Appearance.Options.UseFont = true;
+            this.txt_GiaNhap.Size = new System.Drawing.Size(291, 22);
+            this.txt_GiaNhap.TabIndex = 5;
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("Tahoma", 9.5F);
+            this.labelControl2.Appearance.Options.UseFont = true;
+            this.labelControl2.Location = new System.Drawing.Point(46, 97);
+            this.labelControl2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(102, 16);
+            this.labelControl2.TabIndex = 139;
+            this.labelControl2.Text = "Tên nhà cung cấp";
+            // 
+            // txt_TenHH
+            // 
+            this.txt_TenHH.Location = new System.Drawing.Point(170, 64);
+            this.txt_TenHH.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_TenHH.Name = "txt_TenHH";
+            this.txt_TenHH.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 9.5F);
+            this.txt_TenHH.Properties.Appearance.Options.UseFont = true;
+            this.txt_TenHH.Size = new System.Drawing.Size(291, 22);
+            this.txt_TenHH.TabIndex = 2;
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Tahoma", 9.5F);
+            this.labelControl1.Appearance.Options.UseFont = true;
+            this.labelControl1.Location = new System.Drawing.Point(47, 40);
+            this.labelControl1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(74, 16);
+            this.labelControl1.TabIndex = 138;
+            this.labelControl1.Text = "Mã hàng hóa";
+            // 
+            // txt_MaHH
+            // 
+            this.txt_MaHH.Location = new System.Drawing.Point(170, 34);
+            this.txt_MaHH.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_MaHH.Name = "txt_MaHH";
+            this.txt_MaHH.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 9.5F);
+            this.txt_MaHH.Properties.Appearance.Options.UseFont = true;
+            this.txt_MaHH.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txt_MaHH.Size = new System.Drawing.Size(291, 22);
+            this.txt_MaHH.TabIndex = 1;
+            // 
+            // cmb_NCC
+            // 
+            this.cmb_NCC.EditValue = "Chọn nhà cung cấp";
+            this.cmb_NCC.Location = new System.Drawing.Point(170, 97);
+            this.cmb_NCC.Name = "cmb_NCC";
+            this.cmb_NCC.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.cmb_NCC.Properties.Items.AddRange(new object[] {
+            "NCC1",
+            "NCC2",
+            "NCC3"});
+            this.cmb_NCC.Size = new System.Drawing.Size(180, 20);
+            this.cmb_NCC.TabIndex = 3;
+            // 
+            // frm_NewHangHoa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(773, 468);
+            this.Controls.Add(this.cmb_NCC);
+            this.Controls.Add(this.labelControl7);
+            this.Controls.Add(this.txt_Ghichu);
+            this.Controls.Add(this.btn_Exit);
+            this.Controls.Add(this.btn_Save);
+            this.Controls.Add(this.labelControl10);
+            this.Controls.Add(this.txt_DVT);
+            this.Controls.Add(this.labelControl6);
+            this.Controls.Add(this.labelControl3);
+            this.Controls.Add(this.txt_GiaNhap);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.txt_TenHH);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.txt_MaHH);
+            this.Name = "frm_NewHangHoa";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hàng hóa";
+            ((System.ComponentModel.ISupportInitialize)(this.txt_Ghichu.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txt_DVT.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txt_GiaNhap.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txt_TenHH.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txt_MaHH.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cmb_NCC.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.LabelControl labelControl7;
+        private DevExpress.XtraEditors.TextEdit txt_Ghichu;
+        private DevExpress.XtraEditors.SimpleButton btn_Exit;
+        private DevExpress.XtraEditors.SimpleButton btn_Save;
+        private DevExpress.XtraEditors.LabelControl labelControl10;
+        private DevExpress.XtraEditors.TextEdit txt_DVT;
+        private DevExpress.XtraEditors.LabelControl labelControl6;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private DevExpress.XtraEditors.TextEdit txt_GiaNhap;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.TextEdit txt_TenHH;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.TextEdit txt_MaHH;
+        private DevExpress.XtraEditors.ComboBoxEdit cmb_NCC;
+    }
+}
---
+++ frm_NewHangHoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace QL_Duan
+{
+    public partial class frm_NewHangHoa : DevExpress.XtraEditors.XtraForm
+    {
+        public frm_NewHangHoa( )
+        {
+            InitializeComponent( );
+        }
+    }
+}
